945c50857fb935e5590704032d96460fe7492132
Tubokeyi Micheal <mtubokeyi@gmail.com>
2025-04-16 10:54:37 +0100

    main commit

--- Backend/src/routes/authRoutes.js
+++ Backend/src/routes/authRoutes.js
@@ -1,15 +1,15 @@
 import express from "express"
 import { Signin, Signout, Signup, updateProfile, checkAuth} from "../controllers/authcontrollers.js"
-import protectRoute from "../middleware/protectUpdateRoute.js"
+import protectUpdateRoute from "../middleware/protectUpdateRoute.js"
 const router = express.Router()
 
 
-router.get('/check-auth', protectRoute, checkAuth)
+router.get('/check-auth', protectUpdateRoute, checkAuth)
 
 router.post('/signup', Signup)
 
 router.post('/signin', Signin)
 router.post('/signout', Signout)
 
-router.put('/update-profile', protectRoute, updateProfile)
+router.put('/update-profile', protectUpdateRoute, updateProfile)
 export default router;
