4e8ec22d6369d2277bdcc7b8cac9d184ce60bf57
Tubokeyi Micheal <mtubokeyi@gmail.com>
2025-04-16 13:10:38 +0100

    main commit

--- Frontend/src/components/ProtectedRoute.jsx
+++ Frontend/src/components/ProtectedRoute.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Navigate, useLocation } from 'react-router-dom';
-import { useAuthStore } from '../stores/authStore';
+import { useAuthStore } from '../store/authStore';
 
 const ProtectedRoute = ({ children }) => {
   const location = useLocation();
